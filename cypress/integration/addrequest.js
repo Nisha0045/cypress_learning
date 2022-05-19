a683076bf7e094f9aa566154f765790b45e69e01
Nisha khan <nk5402690@gmail.com>
2022-05-20 00:00:11 +0500

    new  add request code

--- cypress/integration/addrequest.js
+++ cypress/integration/addrequest.js
@@ -1,15 +1,18 @@
 it('Alpha login', () => {
-    cy.visit('https://beta.api.trukkr.pk/login')
+    cy.visit('https://beta.api.trukkr.pk')
     cy.get('#userName').type('zohaib.naqvi@artisticmilliners.com')
     cy.get('#password').type('123456789')
     cy.get('#mui-1').click()
-    cy.get(':nth-child(4) > .c-sidebar-nav-link').click()
+     cy.get(':nth-child(4) > .c-sidebar-nav-link').click()
+     cy.get('#mui-2', { timeout: 5000 }).click({ force: true })
+  
 // cy.get(".cy.get('#mui-2')").click()
 
+// cy.get("#mui-1").click();
 
-cy.get('input[type="button"]').type('book')
-        cy.get('#mui-2').trigger('click')
+// cy.get(".container-fluid button:nth-Add New Request").click();
 
+// cy.get('.MuiButton-root').click()
 
 
 })
