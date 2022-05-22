1045589a6bfb502485b3bf5a6772edbb077281b5
Nisha khan <nk5402690@gmail.com>
2022-05-23 00:01:10 +0500

    new  add request code

--- cypress/integration/addrequest.js
+++ cypress/integration/addrequest.js
@@ -6,13 +6,17 @@ it('Alpha login', () => {
      cy.get(':nth-child(4) > .c-sidebar-nav-link').click()
      cy.get('#mui-2', { timeout: 5000 }).click({ force: true })
   
-// cy.get(".cy.get('#mui-2')").click()
 
-// cy.get("#mui-1").click();
-
-// cy.get(".container-fluid button:nth-Add New Request").click();
-
-// cy.get('.MuiButton-root').click()
+cy.get(':nth-child(1) > .MuiTypography-root > .row > .col-12 > .MuiAutocomplete-root > .MuiFormControl-root > .MuiOutlinedInput-root > #client-dropdown').click()
+cy.get('#client-dropdown-option-1', { timeout: 5000 }).click({ force: true })
+// cy.get('MuiOutlinedInput-input MuiInputBase-input MuiInputBase-inputSizeSmall MuiInputBase-inputAdornedEnd MuiAutocomplete-input MuiAutocomplete-inputFocused css-b52kj1', { timeout: 5000 }).click({ force: true 
+cy.get(':nth-child(2) > .MuiTypography-root > .row > .col-12 > .MuiAutocomplete-root > .MuiFormControl-root > .MuiOutlinedInput-root > #client-dropdown').click()
+cy.get('#client-dropdown-option-2', { timeout: 5000 }).click({ force: true })
+cy.get('#mui-6').click()
+cy.get(':nth-child(4) > :nth-child(5) > .MuiButtonBase-root').click()
+cy.get('#mui-7').click()
+cy.get('.css-1umqo6f').click()
 
 
 })
+// })
