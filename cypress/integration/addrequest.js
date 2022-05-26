1229153459119a570e43f175b9c50cc55e612b3b
Nisha khan <nk5402690@gmail.com>
2022-05-26 15:51:24 +0500

    new  add request new  code

--- cypress/integration/addrequest.js
+++ cypress/integration/addrequest.js
@@ -5,17 +5,25 @@ it('Alpha login', () => {
     cy.get('#mui-1').click()
      cy.get(':nth-child(4) > .c-sidebar-nav-link').click()
      cy.get('#mui-2', { timeout: 5000 }).click({ force: true })
-  
 
 cy.get(':nth-child(1) > .MuiTypography-root > .row > .col-12 > .MuiAutocomplete-root > .MuiFormControl-root > .MuiOutlinedInput-root > #client-dropdown').click()
 cy.get('#client-dropdown-option-1', { timeout: 5000 }).click({ force: true })
-// cy.get('MuiOutlinedInput-input MuiInputBase-input MuiInputBase-inputSizeSmall MuiInputBase-inputAdornedEnd MuiAutocomplete-input MuiAutocomplete-inputFocused css-b52kj1', { timeout: 5000 }).click({ force: true 
 cy.get(':nth-child(2) > .MuiTypography-root > .row > .col-12 > .MuiAutocomplete-root > .MuiFormControl-root > .MuiOutlinedInput-root > #client-dropdown').click()
 cy.get('#client-dropdown-option-2', { timeout: 5000 }).click({ force: true })
 cy.get('#mui-6').click()
 cy.get(':nth-child(4) > :nth-child(5) > .MuiButtonBase-root').click()
 cy.get('#mui-7').click()
 cy.get('.css-1umqo6f').click()
+// cy.get('.MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-vubbuv').click()
+cy.get('[name="request"] > :nth-child(3) > :nth-child(2) > .MuiButton-root' , { timeout: 5000 }).click({ force: true })
+cy.get('#categoryName > .css-o2ohsi-control > .css-1hwfws3', { timeout: 5000 }).click({ force: true })
+cy.get('#categoryName > .css-o2ohsi-control > .css-1hwfws3').click()
+// cy.get('#categoryName > .css-o2ohsi-control > .css-1hwfws3-option-3').click()
+// cy.get('.css-1uccc91-singleValue').click()
+// cy.get('.css-1g6gooi',{timeout:500}).click({force:true})
+cy.get('.css-1uccc91-singleValue> shezore',{timeout:500}).click({force:true})
+cy.get()
+
 
 
 })
--- cypress/integration/approve.js
+++ cypress/integration/approve.js
@@ -1,20 +1,32 @@
+// it('Alpha login', () => {
+//     cy.visit('https://portal-alpha.trukkr.pk/')
+//     //     cy.get(‘input[name=“master@trukkr.pk”]’)
+//     //    .invoke(‘text’)
+//     //    .then(sometext => cy.log(sometext));
+//     // cy.get(‘input[name=“master@trukkr.pk”]‘).then(($text)=>{
+//     //     const txt = $text.text()
+//     //     cy.log(txt)
+//     // })
+//     cy.get('#username', { timeout: 5000 }).type('master@trukkr.pk', { force: true })
+//     cy.get('#password', { timeout: 5000 }).type('king1122', { force: true })
+//     // cy.get(‘.Iinput’, { timeout: 5000 }).click({ multiple: true })
+//     cy.get('.ant-form', { timeout: 5000 }).submit({ multiple: true })
+//     cy.get('.ant-menu-submenu ant-menu-submenu-inline',{timeout:500}).click({ force: true })
+
+
+
+// })
 it('Alpha login', () => {
-        cy.visit('https://portal-alpha.trukkr.pk/')
-
-
-    //     cy.get('input[name="master@trukkr.pk"]')
-    //    .invoke('text')
-    //    .then(sometext => cy.log(sometext));
-
-        // cy.get('input[name="master@trukkr.pk"]').then(($text)=>{
-        //     const txt = $text.text()
-        //     cy.log(txt)
+    cy.visit('https://portal-alpha.trukkr.pk/')
+    cy.get('#username', { timeout: 5000 }).type('master@trukkr.pk', { force: true })
+    cy.get('#password', { timeout: 5000 }).type('king1122', { force: true })
+    // cy.get(‘.Iinput’, { timeout: 5000 }).click({ multiple: true })
+    cy.get('.ant-form', { timeout: 5000 }).submit({ multiple: true })
+    cy.get('.ant-menu-submenu-inline>.ant-menu-submenu-title .ant-menu-submenu-arrow').click({ multiple: true, force: true })
+    // cy.get('.ant-menu-item ant-menu-item-selected').click()
+    // cy.get('.ant-menu-item' ,{ timeout: 5000 }).click({ force: true })
+    cy.get('#UserRoleManagement/Menu>:nth-child(2) > a').click()
     
-        // })
+    
+})
 
-        cy.get('#username').type('master@trukkr.pk')
-        cy.get('#password').type('king1122')
-        cy.get('#mui-6').click()
-    })
-
-   
