7905785caf576f671e7f446d3138cade5ebda87a
NishaKhan987 <nisha.khan.trukkr@gmail.com>
2023-01-29 23:01:05 +0500

    Add vehicle

--- cypress/integration/cypress/addvehicle.js
+++ cypress/integration/cypress/addvehicle.js
@@ -12,10 +12,22 @@ it('Alpha login', () => {
         const threeDigitNumber = Math.floor(Math.random() * 1000);
         result=result+specialCreator+threeDigitNumber;
         return result;
-
-
-    
       } 
+function generateRandomChasisNum(length){
+  var result='';
+  var characters='ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  var charactersLength=characters.length;
+  for ( var i = 0; i < length; i++ ) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+ }
+ const Numbers = Math.floor(Math.random() * 1000);
+ result=result+ Numbers;
+ return result;
+
+}
+
+
+
 
     cy.visit('https://beta.api.trukkr.pk')
 
@@ -29,7 +41,14 @@ it('Alpha login', () => {
     //cy.get(':nth-child(3) > .form-group > #_id > .css-n7p1v0-control > .css-1hwfws3').select('.css-n7p1v0-control').should('have.value','.css-n7p1v0-control');
     cy.get(':nth-child(4) > .form-group > #_id > .css-n7p1v0-control > .css-1hwfws3').should('be.visible').click()
     cy.get('#react-select-4-option-0',{timeout:5000}).click()
-    cy.get('#chasisNo').type()
+    cy.get('#chasisNo',{timeout:500}).type(generateRandomChasisNum(7))
+    cy.get('#engineNo',{timeout:500}).type(generateRandomChasisNum(9))
+    cy.get('#fuelType > .css-n7p1v0-control > .css-1hwfws3',{timeout:500}).click()
+    cy.get('#react-select-6-option-1',{timeout:5000}).click()
+    cy.get(':nth-child(3) > .documents-section > .text-center > .w-100 > .inner-div',{timeout:5000}).click()
+    cy.get('.float-right > svg').click()
+    cy.get(':nth-child(1) > .documents-section > .text-center > .w-100 > .inner-div').click()
+
 
 
 }
